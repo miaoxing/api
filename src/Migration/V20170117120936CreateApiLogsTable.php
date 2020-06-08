b7edd2db97df9afb9c7a43acd386719fe5df4bf7
twinh <twinhuang@qq.com>
2020-06-08 22:47:04 +0800

    feat: Migration 加入 wei 中

--- src/Migration/V20170117120936CreateApiLogsTable.php
+++ src/Migration/V20170117120936CreateApiLogsTable.php
@@ -2,7 +2,7 @@
 
 namespace Miaoxing\Api\Migration;
 
-use Miaoxing\Services\Migration\BaseMigration;
+use Wei\Migration\BaseMigration;
 
 class V20170117120936CreateApiLogsTable extends BaseMigration
 {
