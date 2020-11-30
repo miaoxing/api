73d6bb3da44aa5e845fdb3c3bbd7d05900d178e1
twinh <twinhuang@qq.com>
2020-11-30 15:45:16 +0800

    refactor: 升级数据库迁移语句

--- src/Migration/V20170117120936CreateApiLogsTable.php
+++ src/Migration/V20170117120936CreateApiLogsTable.php
@@ -13,8 +13,8 @@ class V20170117120936CreateApiLogsTable extends BaseMigration
     {
         $this->schema->table('apiLogs')
             ->id()
-            ->int('appId')
-            ->mediumInt('code')->comment('HTTP请求或接口返回的状态码')
+            ->uInt('appId')
+            ->uMediumInt('code')->comment('HTTP请求或接口返回的状态码')
             ->string('url')->comment('请求地址')
             ->text('options')->comment('请求参数')
             ->mediumText('req')
